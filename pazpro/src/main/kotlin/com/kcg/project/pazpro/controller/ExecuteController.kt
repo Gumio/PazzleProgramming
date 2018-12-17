176a641e6842544dc6f4157eb986709dede194b2
Gumio <quran.gumi3134@gmail.com>
2018-12-17 11:32:52 +0900

    add cros annotation to executeController

--- pazpro/src/main/kotlin/com/kcg/project/pazpro/controller/ExecuteController.kt
+++ pazpro/src/main/kotlin/com/kcg/project/pazpro/controller/ExecuteController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*
 class ExecuteController(
     val executeServiceFacade: ExecuteServiceFacade
 ) {
+    @CrossOrigin
     @PostMapping("")
     fun execute(@RequestParam("code_text") plainCodeText: String): String =
 //        val code = "for i in 1..30;if i%15==0;puts "FizzBuzz!";elsif i%3==0;puts "Fizz!";elsif i%5==0;puts "Buzz!";else;puts i;end;end;"
